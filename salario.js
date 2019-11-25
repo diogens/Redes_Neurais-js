7a27d2a83e1e59e56c0a8c01e7140f07e7366cdb
Diogenes <diogensgreen@gmail.com>
2019-11-25 19:18:02 -0300

    exemplo

--- salario.js
+++ salario.js
@@ -21,14 +21,8 @@ rna.train(
   [
     //   1º Anos   |  2º Tipo | 3º Salário
     //     2     |   null   |  R$ 4.000
-    /**Salário - tipo - anos = Saida do Salário */
+    /**Salário - Funcão - Idade = Saida do Salário */
     { input: [0.04, 0.0, 0.34], output: [0.3001] },
-    /* { input: [0.04, 0.0, 0.24], output: [0.3501] },
-    { input: [0.04, 0.0, 0.33], output: [0.3201] },
-    { input: [0.04, 0.0, 0.31], output: [0.4001] },
-    { input: [0.04, 0.0, 0.35], output: [0.5001] },
-    { input: [0.04, 0.0, 0.55], output: [0.1001] }, */
-    /* { input: [0.04, 0.0, 0.6], output: [0.3001] }, */
     { input: [0.02, 0.1, 0.21], output: [0.3501] }
   ],
   { iterations: 100 }
